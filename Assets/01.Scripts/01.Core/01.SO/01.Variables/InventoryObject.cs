635bec501433ee00a8d1cad954510681957237eb
dltnghks <dltrnghks@naver.com>
2025-12-16 17:29:05 +0900

    chore : set SO(ScriptableObject) based architecture

---
+++ Assets/01.Scripts/01.Core/01.SO/01.Variables/InventoryObject.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/System")]
+public class InventoryObject : ScriptableObject
+{
+    // 실제 아이템들이 들어갈 데이터베이스
+    public List<InventorySlot> Container = new List<InventorySlot>();
+
+    public void AddItem(ItemData _item, int _amount)
+    {
+        // 1. 이미 인벤토리에 있는지 확인 (Stackable 체크 등 로직 추가 가능)
+        bool hasItem = false;
+        for (int i = 0; i < Container.Count; i++)
+        {
+            if (Container[i].item == _item)
+            {
+                Container[i].AddAmount(_amount);
+                hasItem = true;
+                break;
+            }
+        }
+        
+        // 2. 없으면 새로 추가
+        if (!hasItem)
+        {
+            Container.Add(new InventorySlot(_item, _amount));
+        }
+    }
+}
---
+++ Assets/01.Scripts/01.Core/01.SO/01.Variables/ItemData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class ItemData : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+    public bool isStackable; // 겹쳐지는 아이템인지?
+    public string description;
+}
