ae3d8b5ca895c26669c50372f0af8c96a68366e8
dltnghks <dltrnghks@naver.com>
2025-12-30 11:04:19 +0900

    chore : Add Package, Font
    - Dotween, TMP 추가
    - TheyPerished 폰트 추가

---
+++ Assets/01.Scripts/01.Core/05.UI/01.GameTitle/GameTitleUI.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+
+public class GameTitleUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro gameTitleText;
+
+    public void Update()
+    {
+        
+    }
+}
