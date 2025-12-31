0e31fce49e4f4d48ef40043acdb2d9310fdccdb4
dltnghks <dltrnghks@naver.com>
2025-12-31 19:24:13 +0900

    feat : 문 열리는 소리

--- Assets/01.Scripts/01.Core/02.InteractObject/Enterence.cs
+++ Assets/01.Scripts/01.Core/02.InteractObject/Enterence.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enterence : MonoBehaviour, IInteractObject
@@ -10,11 +11,15 @@ public class Enterence : MonoBehaviour, IInteractObject
     [SerializeField] private EGameEvent onFailOpenEvent;
     [SerializeField] private EGameEvent onOpenEvent;
 
+    [Header("이벤트 채널")]
+    [SerializeField] private WwiseAudioData openSound;
+
     // 외부(플레이어)에서 호출할 함수
     public void Open()
     {
         if (gameEventChannel != null)
         {
+            AudioManager.Instance.PlayOneShot(openSound, transform.position);
             gameEventChannel.RaiseEvent(onOpenEvent);
         }
         Destroy(gameObject);
